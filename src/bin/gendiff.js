3fc1da692c245d2c12127a4aed16e5a39e9b718d
artem <borman306@yandex.ru>
2019-03-25 23:31:13 +0300

    fix remove custom help(gendiff.js)

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,8 +5,8 @@ import { version } from '../../package.json';
 program
   .version(version)
   .description('Compares two configuration files and shows a difference.')
-  .option('-h, --help', 'output usage information')
-  .option('-V, --version', 'output the version number')
   .option('-f, --format', 'output format')
   .arguments('<firstConfig>', '<secondConfig>')
   .parse(process.argv);
+
+console.log('app is running');
