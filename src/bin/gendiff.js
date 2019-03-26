98f581dcf69f5194f35024d660c1e43deb5b99e6
artem <borman306@yandex.ru>
2019-03-26 22:28:36 +0300

    fix add action (gendiff.js)

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,14 @@
 #! /usr/bin/env node
 import program from 'commander';
 import { version } from '../../package.json';
+import compareConfigs from '..';
 
 program
   .version(version)
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format', 'output format')
   .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => {
+    console.log(compareConfigs(firstConfig, secondConfig));
+  })
   .parse(process.argv);
