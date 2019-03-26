edb0b724a54688813dfa60b8d3add5e14cdbdfdd
artem <borman306@yandex.ru>
2019-03-27 01:00:06 +0300

    optimize gendiff function (index.js)

--- src/index.js
+++ src/index.js
@@ -2,11 +2,6 @@ import { has, uniq } from 'lodash';
 import fs from 'fs';
 import path from 'path';
 
-const signsByOrder = {
-  beforeToAfter: { deleted: '-', added: '+' },
-  afterToBefore: { deleted: '+', added: '-' },
-};
-
 const getPath = pathString => path.resolve(pathString);
 
 export default (filepath1, filepath2) => {
@@ -15,37 +10,29 @@ export default (filepath1, filepath2) => {
 
   const propertiesOfBefore = Object.keys(contentBefore);
   const propertiessOfAfter = Object.keys(contentAfter);
-
-  const [
-    compareFrom,
-    compareTo,
-    orderOfComparisson,
-  ] = propertiesOfBefore.length >= propertiessOfAfter.length
-    ? [contentBefore, contentAfter, 'beforeToAfter'] : [contentAfter, contentBefore, 'afterToBefore'];
-
   const allProperties = uniq([...propertiesOfBefore, ...propertiessOfAfter]);
 
   const arr = allProperties.reduce((acc, property) => {
-    const valueOfFrom = compareFrom[property];
-    const valueOfTo = compareTo[property];
+    const valueOfFrom = contentBefore[property];
+    const valueOfTo = contentAfter[property];
 
-    if (has(compareFrom, property) && has(compareTo, property)) {
+    if (has(contentBefore, property) && has(contentAfter, property)) {
       if (valueOfFrom === valueOfTo) {
         return [...acc, `  ${property}: ${valueOfFrom}`];
       }
 
       return [
         ...acc,
-        `${signsByOrder[orderOfComparisson].deleted} ${property}: ${valueOfFrom}`,
-        `${signsByOrder[orderOfComparisson].added} ${property}: ${valueOfTo}`,
+        `- ${property}: ${valueOfFrom}`,
+        `+ ${property}: ${valueOfTo}`,
       ];
     }
 
-    if (has(compareTo, property)) {
-      return [...acc, `${signsByOrder[orderOfComparisson].added} ${property}: ${valueOfTo}`];
+    if (has(contentAfter, property)) {
+      return [...acc, `+ ${property}: ${valueOfTo}`];
     }
 
-    return [...acc, `${signsByOrder[orderOfComparisson].deleted} ${property}: ${valueOfFrom}`];
+    return [...acc, `- ${property}: ${valueOfFrom}`];
   }, []);
 
   return `{\n  ${arr.join('\n  ')}\n}`;
