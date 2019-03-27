5a4cca88dac4628a42bee41010d0681e864b6502
artem <borman306@yandex.ru>
2019-03-27 22:43:55 +0300

    add format description for tests (gendiff.test.js)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -5,11 +5,11 @@ describe('sould get diff right', () => {
   const expected = fs.readFileSync('__tests__/__fixtures__/expectedResult', 'utf-8');
 
   test.each([
-    ['__tests__/__fixtures__/JSON/before.json', '__tests__/__fixtures__/JSON/after.json', expected],
-    ['__tests__/__fixtures__/yaml/before.yml', '__tests__/__fixtures__/yaml/after.yml', expected],
-    ['__tests__/__fixtures__/ini/before.ini', '__tests__/__fixtures__/ini/after.ini', expected],
-  ])('testing different formats',
-    (filepath1, filepath2, expectedResult) => {
+    ['JSON', '__tests__/__fixtures__/JSON/before.json', '__tests__/__fixtures__/JSON/after.json', expected],
+    ['yaml', '__tests__/__fixtures__/yaml/before.yml', '__tests__/__fixtures__/yaml/after.yml', expected],
+    ['ini', '__tests__/__fixtures__/ini/before.ini', '__tests__/__fixtures__/ini/after.ini', expected],
+  ])('testing %s format',
+    (ext, filepath1, filepath2, expectedResult) => {
       expect(genDiff(filepath1, filepath2)).toBe(expectedResult);
     });
 });
