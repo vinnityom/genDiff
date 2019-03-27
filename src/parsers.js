f089788e0d1d2b226e9e95a6c6ce90f7c8cbf6f7
artem <borman306@yandex.ru>
2019-03-27 22:48:04 +0300

    add ini parser (parsers.js)

--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,10 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parsers = {
   '.yml': content => yaml.safeLoad(content),
   '.json': content => JSON.parse(content),
+  '.ini': content => ini.parse(content),
 };
 
 export default ext => parsers[ext];
