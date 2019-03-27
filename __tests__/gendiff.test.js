a71b97c55e76e018977a9bb56e974699b1bb720a
artem <borman306@yandex.ru>
2019-03-27 22:33:39 +0300

    add test for ini (gendiff.test.js)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,11 +4,12 @@ import genDiff from '../src';
 describe('sould get diff right', () => {
   const expected = fs.readFileSync('__tests__/__fixtures__/expectedResult', 'utf-8');
 
-  test('JSON', () => {
-    expect(genDiff('__tests__/__fixtures__/JSON/before.json', '__tests__/__fixtures__/JSON/after.json')).toBe(expected);
-  });
-
-  test('yaml', () => {
-    expect(genDiff('__tests__/__fixtures__/yaml/before.yml', '__tests__/__fixtures__/yaml/after.yml')).toBe(expected);
-  });
+  test.each([
+    ['__tests__/__fixtures__/JSON/before.json', '__tests__/__fixtures__/JSON/after.json', expected],
+    ['__tests__/__fixtures__/yaml/before.yml', '__tests__/__fixtures__/yaml/after.yml', expected],
+    ['__tests__/__fixtures__/ini/before.ini', '__tests__/__fixtures__/ini/after.ini', expected],
+  ])('testing different formats',
+    (filepath1, filepath2, expectedResult) => {
+      expect(genDiff(filepath1, filepath2)).toBe(expectedResult);
+    });
 });
