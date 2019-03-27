568fab8479169e6fcdcaa8a0c40b4db3ae31b287
artem <borman306@yandex.ru>
2019-03-27 21:07:39 +0300

    refactor put filereading inside test function (jsondiff.test.js)

--- __tests__/jsondiff.test.js
+++ __tests__/jsondiff.test.js
@@ -1,8 +1,7 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-const expected = fs.readFileSync('__tests__/__fixtures__/expectedResult', 'utf-8');
-
 test('should get diff right', () => {
+  const expected = fs.readFileSync('__tests__/__fixtures__/expectedResult', 'utf-8');
   expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toBe(expected);
 });
