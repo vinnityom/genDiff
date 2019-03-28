8a041e83d6a6890347f8a07e9e126e3fea567859
artem <borman306@yandex.ru>
2019-03-29 01:11:48 +0300

    add render module (render.js) does not work yet

--- src/render.js
+++ src/render.js
@@ -1,17 +1,41 @@
-const signs = {
+import _ from 'lodash';
+
+const sign = {
   unchanged: ' ',
   added: '+',
   deleted: '-',
 };
 
+const getValueString = (value, shift) => {
+  if (!_.isObject(value)) {
+    return value;
+  }
+
+  const strings = _.keys(value).map(key => `${shift}${key}: ${value[key]}`);
+  return `{\n${shift}${strings.join(`\n${shift}`)}\n${shift}  }`;
+};
+
 export default (ast) => {
-  const arrayOfStrings = ast.map((element) => {
-    if (element.status === 'changed') {
-      return `- ${element.property}: ${element.valueBefore}\n  + ${element.property}: ${element.valueAfter}`;
-    }
+  const shift = times => '  '.repeat(times);
+  const genOutput = (arr, depth) => {
+    const strings = arr.map((element) => {
+      const {
+        property, status, value, children,
+      } = element;
 
-    return `${signs[element.status]} ${element.property}: ${element.value}`;
-  });
+      if (children) {
+        return `${shift(depth)}${sign[status]} ${property}: ${genOutput(children, depth + 1)}`;
+      }
 
-  return `{\n  ${arrayOfStrings.join('\n  ')}\n}`;
+      if (status === 'changed') {
+        return `${shift(depth)}- ${property}: ${getValueString(value.before, shift(depth + 1))}\n${shift(depth + 1)}+ ${property}: ${getValueString(value.after, shift(depth + 1))}`;
+      }
+
+      return `${shift(depth)}${sign[status]} ${property}: ${getValueString(value, shift(depth + 2))}`;
+    });
+
+    return `{\n${shift(depth - 1)}${strings.join('\n  ')}\n}`;
+  };
+
+  return genOutput(ast, 1);
 };
