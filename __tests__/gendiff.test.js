99b49919c1bbc44aa14b5f9ff3468face6f1d61a
artem <borman306@yandex.ru>
2019-03-30 18:29:40 +0300

    add tests for plain format(gendiff.test.js)

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,6 +10,19 @@ describe('sould get diff right', () => {
     ['ini', '__tests__/__fixtures__/ini/before.ini', '__tests__/__fixtures__/ini/after.ini', expected],
   ])('testing %s format',
     (ext, filepath1, filepath2, expectedResult) => {
-      expect(genDiff(filepath1, filepath2)).toBe(expectedResult);
+      expect(genDiff(filepath1, filepath2, 'jsonLike')).toBe(expectedResult);
+    });
+});
+
+describe('should make plain format right', () => {
+  const expected = fs.readFileSync('__tests__/__fixtures__/expectedPlainOutput', 'utf-8');
+
+  test.each([
+    ['JSON', '__tests__/__fixtures__/JSON/before.json', '__tests__/__fixtures__/JSON/after.json', expected],
+    ['yaml', '__tests__/__fixtures__/yaml/before.yml', '__tests__/__fixtures__/yaml/after.yml', expected],
+    ['ini', '__tests__/__fixtures__/ini/before.ini', '__tests__/__fixtures__/ini/after.ini', expected],
+  ])('testing %s format',
+    (ext, filepath1, filepath2, expectedResult) => {
+      expect(genDiff(filepath1, filepath2, 'plain')).toBe(expectedResult);
     });
 });
