33a3b134b58decc8c24ae106d0682580fa1c43c5
artem <borman306@yandex.ru>
2019-03-31 12:17:08 +0300

    add json renderer

---
+++ src/renderers/renderJSON.js
@@ -0,0 +1,3 @@
+export default (diff) => {
+  console.log('json');
+};
--- src/renderersFacade.js
+++ src/renderersFacade.js
@@ -1,9 +1,11 @@
 import renderCascade from './renderers/renderCascade';
 import plainRender from './renderers/renderPlain';
+import renderJSON from './renderers/renderJSON';
 
 const renderMethods = {
   plain: plainRender,
   cascade: renderCascade,
+  json: renderJSON,
 };
 
 export default (diff, format) => renderMethods[format](diff);
